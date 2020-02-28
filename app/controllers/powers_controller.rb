b036950dadd2619b92e67e52810d1b2e45b328d1
Fika98 <Afzalov98@gmail.com>
2020-02-28 11:20:58 -0500

    code 2

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,22 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show 
+    @power = Power.find(params[:id])
+  end 
+
+  def edit 
+    @power = Power.find(params[:id])
+  end 
+  def update
+    @power = Power.find(params[:id])
+    @power.update(power_params)
+    redirect_to power_path(@power)
+  end 
+
+private  
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end 
 end
