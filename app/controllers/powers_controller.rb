02461210651112919b174a75959062d55dbde543
mav-code <mavisansmembres@gmail.com>
2020-02-28 11:17:38 -0500

    finish CC

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,28 @@
 class PowersController < ApplicationController
+  
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.all.find(params[:id])
+  end
+
+  def edit
+    @power = Power.all.find(params[:id])
+  end
+
+  def update
+    @power = Power.all.find(params[:id])
+    if article.update(power_params)
+      redirect_to @power
+
+    #this is where i am working when time is called
+  end
+
+  private
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end
+
 end
